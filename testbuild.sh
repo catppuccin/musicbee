fc4c4c33e4067ec5e716edc1102e22293cc6ca43
James McKee <james@mck.is>
2023-05-06 15:40:16 +0100

    Build script: Now also update accent colour

--- testbuild.sh
+++ testbuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-theme_name='mocha'
 theme_names=("mocha" "macchiato" "frappe" "latte")
 palette_names=("rosewater" "flamingo" "pink" "mauve" "red" "maroon" "peach" "yellow" "green" "teal" "sky" "sapphire" "blue" "lavender")
+palettes=("244,219,214" "240,198,198" "245,189,230" "198,160,246" "237,135,150" "238,153,160" "245,169,127" "238,212,159" "166,218,149" "139,213,202" "145,215,227" "125,196,228" "138,173,244" "183,189,248")
 
 
 check_exit () {
@@ -33,6 +33,7 @@ for theme_index in {0..3}; do
 
         sed -i "s/theme-palette-placeholder/theme-${theme_names[$theme_index]}/g"   ./catppuccin-base.xml ./skin_wavebar.xml ./skin.xml ./skin.bak
         sed -i "s/accent-placeholder/accent-${palette_names[$colour_index]}/g"      ./catppuccin-base.xml ./skin_wavebar.xml ./skin.xml ./skin.bak
+        sed -i "s/Accent=\"placeholder\"/Accent=\"${palettes[$colour_index]}\"/g"   ./catppuccin-base.xml
 
         xdotool key Enter
         xdotool key Tab
@@ -57,6 +58,7 @@ for theme_index in {0..3}; do
 
         sed -i "s/theme-${theme_names[$theme_index]}/theme-palette-placeholder/g"   ./catppuccin-base.xml ./skin_wavebar.xml ./skin.xml ./skin.bak
         sed -i "s/accent-${palette_names[$colour_index]}/accent-placeholder/g"      ./catppuccin-base.xml ./skin_wavebar.xml ./skin.xml ./skin.bak
+        sed -i "s/Accent=\"${palettes[$colour_index]}\"/Accent=\"placeholder\"/g"   ./catppuccin-base.xml
     done
 done
 
