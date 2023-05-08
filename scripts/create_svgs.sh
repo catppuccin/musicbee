1bce1f307e16ba69b603d8ed90c57ec577f45e81
James McKee <james@mck.is>
2023-05-08 17:19:06 +0100

    Update create svgs script

--- scripts/create_svgs.sh
+++ scripts/create_svgs.sh
@@ -10,7 +10,10 @@ theme_names=("latte" "frappe" "macchiato" "mocha")
 accent_names=("rosewater" "flamingo" "pink" "mauve" "red" "maroon" "peach" "yellow" "green" "teal" "sky" "sapphire" "blue" "lavender")
 
 mantles=("e6e9ef" "292c3c" "1e2030" "181825")
+crusts=("dce0e8" "232634" "181926" "11111b")
 surface1s=("bcc0cc" "51576d" "494d64" "45475a")
+subtext0s=("6c6f85" "a5adce" "a5adcb" "a6adc8")
+texts=("4c4f69" "c6d0f5" "cad3f5" "cdd6f4")
 
 latte_accents=("dc8a78" "dd7878" "ea76cb" "8839ef" "d20f39" "e64553" "fe640b" "df8e1d" "40a02b" "179299" "04a5e5" "209fb5" "1e66f5" "7287fd")
 frappe_accents=("f2d5cf" "eebebe" "f4b8e4" "ca9ee6" "e78284" "ea999c" "ef9f76" "e5c890" "a6d189" "81c8be" "99d1db" "85c1dc" "8caaee" "babbf1")
@@ -19,7 +22,7 @@ mocha_accents=("f5e0dc" "f2cdcd" "f5c2e7" "cba6f7" "f38ba8" "eba0ac" "fab387" "f
 
 for file in "${files[@]}"; do
     echo $file
-    for i in {0..3}; do
+    for i in "${!theme_names[@]}"; do
         echo ${theme_names[$i]}
         current_palette_name=${theme_names[$i]}_accents
         declare -n accents="$current_palette_name"
@@ -28,7 +31,7 @@ for file in "${files[@]}"; do
         sed -i "s/1e2030/${mantles[i]}/g" ./$file.svg
         sed -i "s/494d64/${surface1s[i]}/g" ./$file.svg
 
-        for j in {0..13}; do
+        for j in "${!accent_names[@]}"; do
             sed -i "s/c6a0f6/${accents[j]}/g" ./$file.svg
             inkscape ./$file.svg -o ./output/${file}_theme-${theme_names[i]}_accent-${accent_names[j]}.png
             sed -i "s/${accents[j]}/c6a0f6/g" ./$file.svg
@@ -40,3 +43,34 @@ for file in "${files[@]}"; do
 done
 
 mv ./output/* ../Images/Others/
+
+cd ./Controls
+rm -r ./output
+mkdir ./output
+
+for file in *; do
+    if test -f $file; then
+        echo $file
+        for i in "${!theme_names[@]}"; do
+            echo "${theme_names[i]}"
+            current_palette_name=${theme_names[$i]}_accents
+            declare -n accents="$current_palette_name"
+
+            sed -i "s/1e2030/${mantles[i]}/g" "./$file"
+            sed -i "s/494d64/${surface1s[i]}/g" "./$file"
+            sed -i "s/181926/${crusts[i]}/g" "./$file"
+            sed -i "s/a5adcb/${subtext0s[i]}/g" "./$file"
+            sed -i "s/cad3f5/${texts[i]}/g" "./$file"
+
+            inkscape "./$file" -o "./output/${file::-4}_theme-${theme_names[i]}.png"
+
+            sed -i "s/${mantles[i]}/1e2030/g" "./$file"
+            sed -i "s/${surface1s[i]}/494d64/g" "./$file"
+            sed -i "s/${crusts[i]}/181926/g" "./$file"
+            sed -i "s/${subtext0s[i]}/a5adcb/g" "./$file"
+            sed -i "s/${texts[i]}/cad3f5/g" "./$file"
+        done
+    fi
+done
+
+
